3e08ed83ccf5c10b344d800f287d2c5abb236fdd
stomybexy <jonatan.sombie@gmail.com>
2016-03-11 21:22:36 +0000

    Update package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'jonatan:bower',
     version: '0.0.1',
     // Brief, one-line summary of the package.
-    summary: 'Add bower dependencies in meteor package.',
+    summary: 'Add bower dependencies in meteor packages.',
     // URL to the Git repository containing the source code for this package.
     git: 'https://github.com/stomybexy/meteor-bower.git',
     // By default, Meteor will default to using README.md for documentation.
