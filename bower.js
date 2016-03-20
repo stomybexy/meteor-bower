29d2c9b4870769070e4c09f925f2ae004130fc1f
jonatan <jonatan.sombie@pactilis.com>
2016-03-20 03:22:23 +0000

    Add NO_BOWER_INSTALL env var option to disable dependencies install

--- bower.js
+++ bower.js
@@ -8,22 +8,31 @@ function BowerCompiler() {
 }
 
 BowerCompiler.prototype.processFilesForTarget = function (files) {
+    if(process.env.NO_BOWER_INSTALL){
+        console.log('Skipping bower dependencies install...');
+        return;
+    }
     var globalCfg = {}; // Get .bowerrc
     try {
         globalCfg = JSON.parse(fs.readFileSync('public/.bowerrc', 'utf8'));
-        console.log(globalCfg);
+        // console.log(globalCfg);
     } catch (e) {
-        console.log(e)
+        // console.log(e)
         globalCfg = {}
     }
 
     var directory = globalCfg.directory || ('/public' + '/bower_components');
+    console.log("Installing bower dependencies into", directory ,"... ");
     files.forEach(function (file) {
-        console.log("Installing bower dependencies ... ");
+        
         try {
             var config = JSON.parse(file.getContentsAsString());
         } catch (e) {
-            console.err("failed to parse bower.json ", e);
+            // console.log("failed to parse bower.json ", e);
+            file.error({
+                message: 'failed to parse bower.json : ' + e
+            });
+            return;
         }
 
 
@@ -43,20 +52,26 @@ BowerCompiler.prototype.processFilesForTarget = function (files) {
 
 
         if (dependencies.length) {
-            
+            var errors = []
             // Try to install packages offline first.
             try {
                 Bower.install(dependencies, { offline: true, save: true, forceLatest: true }, { directory: directory });
 
             } catch (e) {
-                console.log(e);
+                errors.push(e);
+                // console.log(e);
 
                 // In case of failure, try to fetch packages online
                 try {
                     Bower.install(dependencies, { save: true, forceLatest: true }, { directory: directory });
                 } catch (f) {
-                    console.log(f);
+                    errors.push(f);
+                    // console.log(errors); 
+                    file.error({
+                        message: 'error installing bower dependencies : ' + errors
+                    });
                 }
+                
             }
 
         };
